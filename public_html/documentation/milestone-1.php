e96b7d7379c59fcac780bf40790e740d2b943535
Dylan Smith <Dylansmithcg@gmail.com>
2020-03-20 04:10:04 -0700

    testing

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>PWP</title>
+</head>
+<body>
+<h1>My PWP</h1>
+<p>Purpose: I want my personal web page to show who I am, and what im about and what I've done.
+</p>
+<p>Audience: Anyone interested in me and what I do, this includes peers, developers, and especially potential employers.
+</p>
+<p>Goal: Display a gallery of my experience and my work to interest employers and acquire an internship/full-time position.
+</p>
+<h2>Persona:</h2>
+<img src="https://ak5.picdn.net/shutterstock/videos/1023851905/thumb/1.jpg" alt="Justin">
+<p><strong>Name:</strong> Justin Sanchez</p>
+<p><strong>Age:</strong> 28</p>
+<p><strong>Sex:</strong> Male</p>
+<p><strong>Location:</strong> Albuquerque, NM</p>
+<p><strong>Job Title:</strong> Hiring Manager at Squarespace</p>
+<p><strong>Technology:</strong> IPhone 11pro, MAC, Touchpad and pen, php storm, when I work App</p>
+<p><strong>Personality:</strong> Confident, witty, alert</p>
+<p><strong>Status:</strong> Married</p>
+<p><strong>Needs/Goals:</strong> Ability to see enough examples of my work to gauge how experienced I am with website development</p>
+<p><strong>Frustrations:</strong> Doesnt usually see websites with links to social media to further examine potential employees</p>
+<p><strong>Description:</strong>Justin Sanchez is a hispanic 28 year old guy originally from Houston, He moved to albuquerque 3 years ago. He use to work for a small firm and now hes a hiring manager for Square Space. Justin has a wife and a son who is 4 years old, he values family and takes care of himself by working out and having good hygiene. Justin is attentive and likes to really focus on his work so he expects the same out of anyone he decides to hire. Justin has hired employees before from Deep dive and at the end of cohort he views each of their resumes and interviews them. He also checks their work and that includes their personal website or work they've made. He likes having the ease of clicking on their other profiles on social media to see what else they enjoy doing. Justin is always on the look out for bright new employees who know how code and have an eye for creative styling.
+</p>
+</body>
+</html>
