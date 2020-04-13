243d51051da07cc7f5ba4958061a7b87ce4f1559
Dylan Smith <Dylansmithcg@gmail.com>
2020-04-13 04:03:45 -0700

    Finished phase 2 maybe?

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>PWP</title>
+</head>
+<body>
+<h1>User story:</h1>
+<p>As a user I want to be able to quickly be able to tell if I want to hire the person based on their website/portfolio.</p>
+<h2>Content strategy:</h2>
+<p><strong>Welcome/ about me:</strong> I want this to be a quick welcome to the page, and a quick description of my background and what experience and hobbies I have, as well as what im planning to do in the future. I Might change the way the look of the website looks depending on if I want to add mroe stuff other than a portfolio.
+</p>
+<p><strong>Portfolio:</strong> I want to maybe put some of my graphics and future side projects in here as well as the finished capstone from the fullstack boot camp. I want each pic to have a thumbnail and then be able to zoom in to see the full picture.</p>
+<p><strong>Contacts:</strong> These will be links to my social media and linked in as well as others.</p>
+<img src="pwp2wireframe.png">
+</body>
+</html>
